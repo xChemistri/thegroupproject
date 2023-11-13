c2452e68bcfa4872463c0f36624ba4334289be1d
Antonio José Velez Agosto <iamphyxics@gmail.com>
2023-11-13 01:49:12 -0500

    adding values

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ function flightselected (value)
     switch (value)
     {
         case "JetBlue":
-            document.getElementById("carryOnSize").innerHTML = "N/A";
-            document.getElementById("personalItemSize").innerHTML = "N/A";
-            document.getElementById("maxWeight").innerHTML = "N/A";
+            document.getElementById("carryOnSize").innerHTML = "22in x 14in x 9in";
+            document.getElementById("personalItemSize").innerHTML = "17in x 13in x 8in";
+            document.getElementById("maxWeight").innerHTML = "50lb";
             break;
         case "United":
-            document.getElementById("carryOnSize").innerHTML = "N/A";
-            document.getElementById("personalItemSize").innerHTML = "N/A";
-            document.getElementById("maxWeight").innerHTML = "N/A";
+            document.getElementById("carryOnSize").innerHTML = "22in x 14in x 9in";
+            document.getElementById("personalItemSize").innerHTML = "17in x 10in x 9in";
+            document.getElementById("maxWeight").innerHTML = "50lb";
             break;
         case "Delta":
-            document.getElementById("carryOnSize").innerHTML = "N/A";
-            document.getElementById("personalItemSize").innerHTML = "N/A";
-            document.getElementById("maxWeight").innerHTML = "N/A";
+            document.getElementById("carryOnSize").innerHTML = "22in x 14 x 9in";
+            document.getElementById("personalItemSize").innerHTML = "17in x 13in x 9in";
+            document.getElementById("maxWeight").innerHTML = "50lb";
             break;
         case "American":
-            document.getElementById("carryOnSize").innerHTML = "N/A";
-            document.getElementById("personalItemSize").innerHTML = "N/A";
-            document.getElementById("maxWeight").innerHTML = "N/A";
+            document.getElementById("carryOnSize").innerHTML = "22in x 14in x 9in";
+            document.getElementById("personalItemSize").innerHTML = "18in x 14in x 8in";
+            document.getElementById("maxWeight").innerHTML = "50lb";
             break;
         case "SouthWest":
-            document.getElementById("carryOnSize").innerHTML = "N/A";
-            document.getElementById("personalItemSize").innerHTML = "N/A";
-            document.getElementById("maxWeight").innerHTML = "N/A";
+            document.getElementById("carryOnSize").innerHTML = "22in x 16in x 10in";
+            document.getElementById("personalItemSize").innerHTML = "18.5in x 8.5in x 13.5in";
+            document.getElementById("maxWeight").innerHTML = "50lb";
             break;
         default:
             document.getElementById("carryOnSize").innerHTML = "N/A";
